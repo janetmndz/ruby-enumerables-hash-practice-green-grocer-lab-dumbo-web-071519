3ff4f298899d6c29c4e8f15efe90c514ff158605
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 01:38:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,51 @@
 def consolidate_cart(cart)
-  # code here
+ 
+  ### using reduce to return a single hash of all items
+  cart.reduce({}) { |memo, item|
+    ### using each to acess all of the inner hash
+    item.each { |(key, value)|
+      memo[key] = value
+      ### if count doesn't already exist, add it and set it to one
+      if !memo[key][:count]
+        memo[key][:count] = 1 
+      ### if count does exist, accumilated it by one
+      else
+        memo[key][:count] += 1
+      end
+    }
+    memo
+  }
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each { |coupon|
+    ### variable setup for readablity 
+    item_name = coupon[:item]
+    
+    ### if this coupon can be applied AND we have enough items for it to apply
+    if cart.keys.include?(item_name) && (cart[item_name][:count] >= coupon[:num])
+      
+      ### variable setup for readablity 
+      couponed_item = "#{item_name} W/COUPON"
+      
+      ### if we already used the same coupon and have another, update the hash
+      if cart[couponed_item]
+        
+        cart[couponed_item][:count] += coupon[:num]
+      ### if this is our first time using this coupon, create a new hash
+      else
+        cart[couponed_item] = {
+          count: coupon[:num],
+          price: coupon[:cost]/coupon[:num],
+          clearance: cart[item_name][:clearance]
+        }
+      end
+      
+      ### update the count in our cart now that a coupon is applied
+      cart[item_name][:count] -= coupon[:num]
+    end
+  }
+  cart
 end
 
 def apply_clearance(cart)
