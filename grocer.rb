71e34a4a9d3d27d50ded7310a1e735e7651c58b7
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 02:05:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,6 +60,5 @@ def checkout(cart, coupons)
   checkout_cart = apply_clearance(checkout_cart)
   checkout_cart.reduce(0) {|total, (key, value)|
     total += checkout_cart[key][:price] * checkout_cart[key][:count]
-    total
   }
 end
